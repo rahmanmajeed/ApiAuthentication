5b5be9430b389aae4f881ea3472639d9c971b617
Majeed <rahmanmajeed46@gmail.com>
2018-05-30 01:52:31 +0600

    middleware,filter roles in user model added

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        /**
+         * Here, $request->user()===null check the user() is logged in or not.
+         */
+        if($request->user()===null)
+        {
+            return response('Insufficient Permission',401);
+        }
+
+        $actions=$request->route()->getAction();
+        $roles=isset($actions['roles']) ? $actions['roles'] : null;
+        if($request->user()->hasAnyRole($roles) || !$roles)
+        {
+            return $next($request);
+        }
+       return response('Insufficient Permission',401);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -32,4 +32,47 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class,'user_roles');
     }
+
+     //===== This is the one way to ACL with middleware =====//
+    /**
+     * hasAnyRole methods parameter $roles recieve data from CheckRole middleware 
+     * check the $roles parameter is it array or not if is_array() return true
+     * then check every role in the array using foreach loop and send the every single role
+     * value to hasRole method.
+     */
+    public function hasAnyRole($roles)
+    {
+        if(is_array($roles))
+        {
+            foreach($roles as $role)
+            {
+                if($this->hasRole($role))
+                {
+                    return true;
+                }
+            }
+        }
+        else
+        {
+            if( $this->hasRole($roles))
+            {
+              return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * hasRole method recieve a role string value and it check with DB role table value
+     * if it's match then Grant the role otherwise close the given role access. 
+     */
+  
+    public function hasRole($role)
+    {
+        if($this->roles()->where('slug',$role)->first())
+        {
+            return true;
+        }
+        return false;
+    }
 }
